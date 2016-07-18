54170ae673348019f5f21767f7a2d8aa2909fbc4
KolesnikovAlexsandr <mister.sasha.kolesnikov@yandex.ru>
2016-07-18 19:06:27 +0300

    Start server

--- index.js
+++ index.js
@@ -2,16 +2,19 @@
  * Created by sasha on 17/07/16.
  */
 var http = require("http");
-var i = 0;
+var url = request("url");
+var counter_of_clients = 0;
 
-function onRequest(reguest,response) {
-    i++;
-    console.log("Request received "+ i);
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hello World");
-    response.end();
+function start() {
+
+    function onRequest(reguest, response) {
+        counter_of_clients++;
+        console.log("Request received " + i);
+        response.writeHead(200, {"Content-Type": "text/plain"});
+        response.write("Hello World");
+        response.end();
+    }
+
+    http.createServer(onRequest).listen(8888);
+    console.log("Start Server");
 }
-
-http.createServer(onRequest).listen(8888);
-
-console.log("Start Server");
---
+++ server.js
@@ -0,0 +1,5 @@
+/**
+ * Created by sasha on 18/07/16.
+ */
+var server = request("./server");
+server.start();
