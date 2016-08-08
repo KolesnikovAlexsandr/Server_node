cb7e10a40227a3a9422e64ae232c10f60fa74fcf
KolesnikovAlexsandr <mister.sasha.kolesnikov@yandex.ru>
2016-08-08 03:47:48 +0300

    add voice recoding and answer

--- spechRecord.js
+++ spechRecord.js
@@ -17,33 +17,51 @@ recognizer.continuous = true;
 // Какой язык будем распознавать?
 recognizer.lang = 'en-US';
 
-function speech(string) {
-    recognizer.stop();
-    var utterance = new SpeechSynthesisUtterance(string);
-    utterance.voice = voices[1];
-    speechSynthesis.speak(utterance);
-    recognizer.start();
-};
+function speech(text) {
+    // Create a new instance of SpeechSynthesisUtterance.
+    var msg = new SpeechSynthesisUtterance();
+
+    // Set the text.
+    msg.text = text;
+
+    // Set the attributes.
+    msg.volume = parseFloat(100);
+    msg.rate = parseFloat(5);
+    msg.pitch = parseFloat(5);
+
+    // If a voice has been selected, find the voice and set the
+    // utterance instance's voice attribute.
+        msg.voice = speechSynthesis.getVoices().filter(function(voice) { return voice.name == "Veena"; })[0];
+
+
+    // Queue this utterance.
+    window.speechSynthesis.speak(msg);
+}
 
 // Используем колбек для обработки результатов
 recognizer.onresult = function (event) {
     var result = event.results[event.resultIndex];
     if (result.isFinal) {
-
-        if(result.isFinal == "friday" && !callProgram)
+        var cmd = result[0].transcript;
+        cmd.toString();
+        cmd.toLowerCase();
+        console.log('Финальный результат:'+cmd);
+        if( cmd == "Friday" && !callProgram)
         {
+            recognizer.stop();
             callProgram == true;
             console.log("call program");
             speech("Hi! How are you today?");
         }
-        else {
-
-            speech(result[0].transcript);
+        else if(callProgram == true)
+        {
+            console.log("call program2");
+            speech("spech something");
         }
-
     } else {
         console.log('Промежуточный результат: ', result[0].transcript);
     }
+    recognizer.stop();
 };
 
 recognizer.onerror = function(event) {
